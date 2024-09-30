b47a62e15fe31f632380e1122f9b492776638bdc
vlad <ecscalibur_10@mail.ru>
2024-09-30 21:28:28 +0300

    added neural network

--- task_5.py
+++ task_5.py
@@ -2,23 +2,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-W11 = np.array([[1, 1, -1.5], [1, 1, -0.5], [1, 1, -1]])
-W21 = np.array([[-1, 1, -0.5], [-1, 1, 0.5], [-1, 1, 0]])
+W1 = np.array([[1, 1, -1.5], [1, 1, -0.5], [1, 1, -1]])
+# W1 = np.array([[-1, 1, -0.5], [-1, 1, 0.5], [-1, 1, 0]])
 W2 = np.array([1, 1, -1])
 
 XX = [np.array([np.random.sample(), np.random.sample(), 1]).T for _ in range(100)]
 
 
-def forwardNN1(x):
-    z1 = W11.dot(x)
-    y1 = np.heaviside(z1, 0)
-    z2 = W2.dot(y1)
-    y2 = np.heaviside(z2, 0)
-    return y2
-
-
-def forwardNN2(x):
-    z1 = W21.dot(x)
+def forwardNN(x):
+    z1 = W1.dot(x)
     y1 = np.heaviside(z1, 0)
     z2 = W2.dot(y1)
     y2 = np.heaviside(z2, 0)
@@ -28,15 +20,15 @@ def forwardNN2(x):
 plt.grid()
 d, c = None, None
 for X in XX:
-    if forwardNN1(X) == forwardNN2(X):
+    if forwardNN(X):
         d = plt.scatter(X[0], X[1], color='red')
     else:
         c = plt.scatter(X[0], X[1], color='green')
-plt.plot([0, 0.5], [0.5, 1], color='blue')
+# plt.plot([0, 0.5], [0.5, 1], color='blue')
 plt.plot([0.5, 1], [1, 0.5], color='blue')
-plt.plot([0.5, 1], [0, 0.5], color='blue')
+# plt.plot([0.5, 1], [0, 0.5], color='blue')
 plt.plot([0, 0.5], [0.5, 0], color='blue')
 plt.plot([0, 1], [1, 0], color='blue')
-plt.plot([0, 1], [0, 1], color='blue')
+# plt.plot([0, 1], [0, 1], color='blue')
 plt.legend((d, c), ['собака', 'кошка'])
 plt.show()
