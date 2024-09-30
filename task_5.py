8e1b809ebc444006caae23493ec79633e7ca15cc
vlad <ecscalibur_10@mail.ru>
2024-09-30 22:06:13 +0300

    added neural network

--- task_5.py
+++ task_5.py
@@ -1,34 +1,61 @@
 import numpy as np
-import matplotlib.pyplot as plt
+from matplotlib.pyplot import figure
 
 
-W1 = np.array([[1, 1, -1.5], [1, 1, -0.5], [1, 1, -1]])
-# W1 = np.array([[-1, 1, -0.5], [-1, 1, 0.5], [-1, 1, 0]])
-W2 = np.array([1, 1, -1])
+W11 = np.array([[1, 1, -1.5], [1, 1, -0.5], [1, 1, -1]])
+W12 = np.array([1, 1, -1])
 
-XX = [np.array([np.random.sample(), np.random.sample(), 1]).T for _ in range(100)]
+XX1 = [np.array([np.random.sample(), np.random.sample(), 1]).T for _ in range(100)]
 
+W21 = np.array([[1, 1, -1.5], [-1, -1, 0.5], [-1, 1, -0.5], [1, -1, -0.5]])
+W22 = np.array([1, 1, 1, 1])
 
-def forwardNN(x):
-    z1 = W1.dot(x)
+XX2 = [np.array([np.random.sample(), np.random.sample(), 1]).T for _ in range(100)]
+
+def forwardNN1(x):
+    z1 = W11.dot(x)
     y1 = np.heaviside(z1, 0)
-    z2 = W2.dot(y1)
+    z2 = W12.dot(y1)
     y2 = np.heaviside(z2, 0)
     return y2
 
 
-plt.grid()
+def forwardNN2(x):
+    z1 = W21.dot(x)
+    y1 = np.heaviside(z1, 0)
+    z2 = W22.dot(y1)
+    y2 = np.heaviside(z2, 0)
+    return y2
+
+
+fig1 = figure()
+ax1 = fig1.add_subplot(111)
+ax1.grid()
 c, d = None, None
-for X in XX:
-    if forwardNN(X):
-        c = plt.scatter(X[0], X[1], color='green')
+for X in XX1:
+    if forwardNN1(X):
+        c = ax1.scatter(X[0], X[1], color='green')
     else:
-        d = plt.scatter(X[0], X[1], color='red')
-# plt.plot([0, 0.5], [0.5, 1], color='blue')
-plt.plot([0.5, 1], [1, 0.5], color='blue')
-# plt.plot([0.5, 1], [0, 0.5], color='blue')
-plt.plot([0, 0.5], [0.5, 0], color='blue')
-plt.plot([0, 1], [1, 0], color='blue')
-# plt.plot([0, 1], [0, 1], color='blue')
-plt.legend((c, d), ['кошка', 'собака'])
-plt.show()
+        d = ax1.scatter(X[0], X[1], color='red')
+ax1.plot([0.5, 1], [1, 0.5], color='blue')
+ax1.plot([0, 0.5], [0.5, 0], color='blue')
+ax1.plot([0, 1], [1, 0], color='blue')
+ax1.legend((c, d), ['кошка', 'собака'])
+fig1.show()
+
+
+fig2 = figure()
+ax2 = fig2.add_subplot(111)
+ax2.grid()
+c, d = None, None
+for X in XX2:
+    if forwardNN2(X):
+        c = ax2.scatter(X[0], X[1], color='green')
+    else:
+        d = ax2.scatter(X[0], X[1], color='red')
+ax2.plot([0, 0.5], [0.5, 1], color='blue')
+ax2.plot([0.5, 1], [1, 0.5], color='blue')
+ax2.plot([0.5, 1], [0, 0.5], color='blue')
+ax2.plot([0, 0.5], [0.5, 0], color='blue')
+ax2.legend((c, d), ['кошка', 'собака'])
+fig2.show()
